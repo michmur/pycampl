d3db36e872a097f7070e78d35e014f157f253b2c
michmur <77murawski@gmail.com>
2022-02-22 18:13:37 +0100

    Update totolotek.py

--- modul_0/totolotek.py
+++ modul_0/totolotek.py
@@ -1,7 +1,6 @@
 from random import random
 
 user_numbers = list()
-lotto_numbers = set()
 
 print("Totolotek v 1.0.0")
 print("Podaj sześć liczb")
@@ -10,5 +9,14 @@ for i in range(6):
     number = int(input("Podaj liczbę : "))
     user_numbers.append(number)
 
-for i in range(1,7):
-    lotto_numbers.add(random.randint(1,49))
+
+def generate_lotto_numbers():
+    """Generate set of six numbers
+
+    Returns:
+        set: set of six numbers
+    """
+    lotto_numbers = set()
+    for i in range(1,7):
+        lotto_numbers.add(random.randint(1,49))
+    return lotto_numbers
