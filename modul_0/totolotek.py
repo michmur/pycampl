3dddf0d7487ada7b542bdfcdf1f8c76fbc86f0f5
Michal Murawski <77murawski@gmail.com>
2022-02-22 14:54:13 +0100

    Update totolotek.py

--- modul_0/totolotek.py
+++ modul_0/totolotek.py
@@ -1,8 +1,5 @@
 from random import random
 
-
-import random
-
 user_numbers = list()
 lotto_numbers = set()
 
@@ -15,8 +12,3 @@ for i in range(6):
 
 for i in range(1,7):
     lotto_numbers.add(random.randint(1,49))
-
-print(f"Numery wytypowane przez użytnownika ")
-print(user_numbers)
-print("Szczęśliwe numery")
-print(lotto_numbers)
