5c9b8e90eecbf9432dcf979b4eaa3100111c07e7
Michal Murawski <77murawski@gmail.com>
2022-02-23 14:50:52 +0100

    Update totolotek.py

--- module_0/totolotek.py
+++ module_0/totolotek.py
@@ -15,11 +15,21 @@ def generate_six_numbers():
         _type_: _description_
     """
     numbers = []
-    for i in range(6):
+    while(len(numbers) != 6):
         numbers.append(randint(1,49))
     return numbers
 
 if __name__ == "__main__":
     lotto = generate_six_numbers()
     user = generate_six_numbers()
+    count = 0
+
+    while(True):
+        print(lotto,user)
+        if len(set(lotto)&set(user)) == 6:
+            break
+        else:
+            user = generate_six_numbers()
+            count += 1
+            print(f"Nie udało się trafić szóstki losujemy dalej dla liczb : {user} losowanie numer {count}")
     
