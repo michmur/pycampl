37804e3a8525da27609b6fe5cab9ac54a649a177
Michal Murawski <77murawski@gmail.com>
2022-02-24 13:45:51 +0100

    Update test_totolotek.py

--- module_0/test_totolotek.py
+++ module_0/test_totolotek.py
@@ -1,5 +1,9 @@
-from totolotek import generate_six_numbers
+from totolotek import generate_six_numbers, check_if_you_win
 
 def test_generate_six_numbers():
     score = generate_six_numbers()
+    assert len(score) == 6
+
+def test_check_if_you_win():
+    score = check_if_you_win()
     assert len(score) == 6
