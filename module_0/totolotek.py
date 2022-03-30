bfa537e4e9dc7127f50b678f6bba17e10f3fdbd7
Michal Murawski <77murawski@gmail.com>
2022-03-30 07:42:13 +0200

    Update totolotek.py

--- module_0/totolotek.py
+++ module_0/totolotek.py
@@ -31,18 +31,24 @@ def check_if_you_win():
     count = 0
     control_list = []
 
-    while len(control_list) != 6:
-        control_list = list(set(lotto)&set(user))
-        if 0 < len(control_list) < 6:
-            count += 1
-            print(f"Udało ci się trafić {len(control_list)} liczb {control_list} losowanie nr : {count:,}")
-            user = generate_six_numbers()
-        else:
-            user = generate_six_numbers()
-            count += 1
-    print(f"{lotto} {user}")
-    return f"Udało ci się trafić 6 za {count:,} razem, wydałeś {count*3:,} zł"
+    # while len(control_list) != 6:
+    #     control_list = list(set(lotto)&set(user))
+    #     if 0 < len(control_list) < 6:
+    #         count += 1
+    #         print(f"Udało ci się trafić {len(control_list)} liczb {control_list} losowanie nr : {count:,}")
+    #         user = generate_six_numbers()
+    #     else:
+    #         user = generate_six_numbers()
+    #         count += 1
+    # print(f"{lotto} {user}")
+    # return f"Udało ci się trafić 6 za {count:,} razem, wydałeś {count*3:,} zł"
 
+    while lotto != user:
+        user = generate_six_numbers()
+        print(lotto,user)
+        count += 1
+        
+    return f"Udało ci się trafić 6 za {count:,} razem, wydałeś {count*3:,} zł"
 
 if __name__ == "__main__":
     print(check_if_you_win())
